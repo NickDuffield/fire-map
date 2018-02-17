9d8182635e8b90c47aebc30dc3d53bfea35b7c68
Nick Duffield <hi.nickduffield@gmail.com>
2018-02-17 09:04:49 +0000

    init firebase

---
+++ public/app.js
@@ -0,0 +1,14 @@
+(function(){
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDupH5-oRofLqaP0M3PbDcXzd-e_yjAEow",
+    authDomain: "fire-map-16163.firebaseapp.com",
+    databaseURL: "https://fire-map-16163.firebaseio.com",
+    projectId: "fire-map-16163",
+    storageBucket: "",
+    messagingSenderId: "288074650901"
+  };
+  firebase.initializeApp(config);  
+
+}());
